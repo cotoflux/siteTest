a2cf339d06bbdfc95cbcd86f5d0246e5cb785e92
json-art <cotoflux@hotmail.com>
2020-02-05 15:03:39 +0100

    Modificado config

--- src/config/apiwork.php
+++ src/config/apiwork.php
@@ -3,19 +3,17 @@
 
 
 return [
-    'config'=>[
-        'Site' => [
-            'token' => env('APIWORK_SITE_TOKEN','abcdefghi'),
-            'endpoint' =>  env('APIWORK_SITE_ENDPOINT','url'),
-            'secret' => env('APIWORK_EGO_SECRET','PASSWORD'),
-            'user' => env('APIWORK_EGO_USER','USER'),
+    'Site' => [
+        'token' => env('APIWORK_SITE_TOKEN','abcdefghi'),
+        'endpoint' =>  env('APIWORK_SITE_ENDPOINT',"https://site.electryconsulting.com/api/v1"),
+        'secret' => env('APIWORK_EGO_SECRET','test1234'),
+        'user' => env('APIWORK_EGO_USER','test@test.test'),
 
-        ],
-        'Ego' => [
-            'procedencia' => env('APIWORK_EGO_TOKEN',"abcdefghi"),
-            'endpoint' => env('APIWORK_EGO_ENDPOINT','url'),
-            'secret' => env('APIWORK_EGO_SECRET','PASSWORD'),
-            'user' => env('APIWORK_EGO_USER','USER'),
-        ],
-    ]
+    ],
+    'Ego' => [
+        'token' => env('APIWORK_EGO_TOKEN',"abcdefghi"),
+        'endpoint' => env('APIWORK_EGO_ENDPOINT',"https://site.electryconsulting.com/api/v1"),
+        'secret' => env('APIWORK_EGO_SECRET','PASSWORD'),
+        'user' => env('APIWORK_EGO_USER','USER'),
+    ],
 ];
