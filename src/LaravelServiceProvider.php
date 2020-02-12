16da8664adf7f6e3c4761620a70f90853fed452d
json-art <cotoflux@hotmail.com>
2020-02-12 13:26:33 +0100

    Elimino comandos de service provider

--- src/LaravelServiceProvider.php
+++ src/LaravelServiceProvider.php
@@ -16,23 +16,7 @@ class LaravelServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/config/apiwork.php' => config_path('apiwork.php'),
         ]);
-        /*
-        * Publish the all thecommands to use when installed
-        */
-/*         $this->publishes([
-            __DIR__ . '/app/Console/Commands/TestAllTrue.php' => app_path('\Console\Commands\TestAllTrue.php'),
 
-            __DIR__ . '/app/Console/Commands/TestConfig.php' => app_path('\Console\Commands\TestConfig.php'),
-
-            __DIR__ . '/app/Console/Commands/TestLoginValidToken.php' => app_path('\Console\Commands\TestLoginValidToken.php'),
-
-            __DIR__ . '/app/Console/Commands/TestHdllToken.php' => app_path('\Console\Commands\TestHdllToken.php'),
-
-            __DIR__ . '/app/Console/Commands/TestKeyPassword.php' => app_path('\Console\Commands\TestKeyPassword.php'),
-
-            __DIR__ . '/app/Console/Commands/TestKeyUser.php' => app_path('\Console\Commands\TestKeyUser.php'),
-
-        ]); */
 
         $this->loadRoutesFrom(__DIR__.'/routes/routes.php');
 
